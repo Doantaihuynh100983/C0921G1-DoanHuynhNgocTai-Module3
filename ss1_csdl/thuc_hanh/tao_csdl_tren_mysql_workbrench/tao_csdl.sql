e8f66559a7a16278c5c832529ab3d7f8e6101f72
Doantaihuynh100983 <doantai100983hu@gmail.com>
2021-12-08 16:24:48 +0700

    up bai

---
+++ ss1_csdl/thuc_hanh/tao_csdl_tren_mysql_workbrench/tao_csdl.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE SinhVien;
+use SinhVien;
+CREATE TABLE SinhVienTT(
+	Id int primary key, 
+    LastName varchar(25),
+    FirtName varchar(25)
+);
+insert into SinhVienTT
+value 
+(1,"DOAN","TAI");
