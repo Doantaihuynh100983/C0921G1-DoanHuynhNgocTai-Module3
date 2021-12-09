677c868271eab9b719f8243704296c5dfddf4eeb
Doantaihuynh100983 <doantai100983hu@gmail.com>
2021-12-09 17:22:54 +0700

    up bai

--- ss1_csdl/bai_tap/csdl_quanly_sinhvien/quan_ly_sinh_vien.sql
+++ ss1_csdl/bai_tap/csdl_quanly_sinhvien/quan_ly_sinh_vien.sql
@@ -1,24 +1,32 @@
 CREATE DATABASE quanly_sinhvien;
 use quanly_sinhvien;
 CREATE TABLE class(
-	id int primary key, 
-    Name varchar(25)
+	id int    primary key AUTO_INCREMENT, 
+    `Name` varchar(25)
 );
-insert into class
-value 
-(1,"DOAN");
+insert into class 
+(`Name`)
+values 
+("DOAN"),
+("OK"),
+("Tai");
 
 
 
 CREATE TABLE teacher(
-	id int primary key, 
-    Name varchar(25),
+	id int NOT NULL AUTO_INCREMENT primary key, 
+    `Name` varchar(25),
     age int ,
     conutry varchar(200)
 );
 
 insert into teacher
 value 
-(1,"TAI",30,"DA NANG");
+("TAI",30,"DA NANG");
+insert into teacher
+value 
+("Daon",45,"DA NANG");
+
+
 SELECT * FROM class; 
 SELECT * FROM teacher; 
