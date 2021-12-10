832cf9bc9085c31498b5df00c1fef0fbdb1a1673
Doantaihuynh100983 <doantai100983hu@gmail.com>
2021-12-10 18:58:16 +0700

    up bai

--- furuma/query.sql
+++ furuma/query.sql
@@ -86,18 +86,40 @@ SELECT
     dv.ten_dich_vu,
     dv.dien_tich,
     dv.chi_phi_thue,
-	ldv.ten_loai_dich_vu
-from furuma.loai_dich_vu ldv join furuma.dich_vu dv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
-	  join furuma.hop_dong hd  on dv.ma_dich_vu = hd.ma_dich_vu 
-where dv.ma_dich_vu NOT IN (
-			select hd.ma_dich_vu from  furuma.hop_dong
-            where  MONTH(ngay_lam_hop_dong) <= 3
-)
+    ldv.ten_loai_dich_vu
+FROM
+    furuma.loai_dich_vu ldv
+        JOIN
+    furuma.dich_vu dv ON ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+        JOIN
+    furuma.hop_dong hd ON  hd.ma_dich_vu = dv.ma_dich_vu 
+WHERE
+  dv.ma_dich_vu NOT IN (
+     SELECT 
+            hd.ma_dich_vu
+         FROM
+           furuma.hop_dong
+   WHERE
+		month(hd.ngay_lam_hop_dong) between 1 and 3 
+ )
+group by dv.ten_dich_vu
+order by dv.ma_dich_vu;
+-- ở đâu mà lòi ra thằng số 5 không hiểu nỗi ???
+
+
+
+
+-- Câu 7 :
+-- Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich,
+-- so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại
+-- dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng
+-- chưa từng được khách hàng đặt phòng trong năm 2021.
+
 
 	
 
 
 
--- select * from furuma.hop_dong;
+ select * from furuma.hop_dong;
 
 
