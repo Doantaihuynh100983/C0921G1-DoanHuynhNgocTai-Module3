07441eee6e386a9db5a1eee551bf77cac18bc903
Doantaihuynh100983 <doantai100983hu@gmail.com>
2021-12-13 17:48:14 +0700

    up bai

--- furuma/query.sql
+++ furuma/query.sql
@@ -223,7 +223,7 @@ from furuma.hop_dong hd join furuma.nhan_vien nv on hd.ma_nhan_vien = nv.ma_nhan
 	left join furuma.hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
 where (year(hd.ngay_lam_hop_dong) = 2020 and month(hd.ngay_lam_hop_dong) IN (10 , 11, 12))
         AND (month(hd.ngay_lam_hop_dong) NOT  IN (1,2,3,4,5,6) and year(hd.ngay_lam_hop_dong) = 2020) 
-group by hd.ma_hop_dong
+group by hd.ma_hop_dong;
 
 
 
@@ -231,5 +231,38 @@ group by hd.ma_hop_dong
 -- câu 13 :Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các
 -- Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử
 -- dụng nhiều như nhau).
+select dvdk.ma_dich_vu_di_kem , dvdk.ten_dich_vu_di_kem , sum(hdct.so_luong) 'so_luong' 
+from furuma.dich_vu_di_kem dvdk join furuma.hop_dong_chi_tiet hdct on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+group by hdct.ma_dich_vu_di_kem
+having sum(hdct.so_luong) >= (select max(so_luong) from furuma.hop_dong_chi_tiet);
 
+
+
+-- câu 14 :Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một
+-- lần duy nhất. Thông tin hiển thị bao gồm ma_hop_dong,
+-- ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính
+-- dựa trên việc count các ma_dich_vu_di_kem).
+select hd.ma_hop_dong , ldv.ten_loai_dich_vu , dvdk.ten_dich_vu_di_kem , count(hdct.ma_dich_vu_di_kem)
+from furuma.hop_dong hd join furuma.dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu 
+	join furuma.loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+    join furuma.hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+    join furuma.dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+group by hdct.ma_dich_vu_di_kem
+having count(hdct.ma_dich_vu_di_kem) = 1
+order by hd.ma_hop_dong asc;
+
+
+
+
+-- Câu 15 :Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten,
+-- ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi mới chỉ lập được
+-- tối đa 3 hợp đồng từ năm 2020 đến 2021.
+select nv.ma_nhan_vien , nv.ho_ten , td.ten_trinh_do , bp.ten_bo_phan , nv.so_dien_thoai , nv.dia_chi 
+from  furuma.nhan_vien nv join furuma.trinh_do td on nv.ma_trinh_do = td.ma_trinh_do
+		join furuma.bo_phan bp on nv.ma_bo_phan = bp.ma_bo_phan
+       join furuma.hop_dong hd on hd.ma_nhan_vien = nv.ma_nhan_vien
+ where year(hd.ngay_lam_hop_dong) between 2020 and 2021 
+group by hd.ma_nhan_vien
+having (count(hd.ma_nhan_vien) <=3) 
+order by  hd.ma_nhan_vien;
 
