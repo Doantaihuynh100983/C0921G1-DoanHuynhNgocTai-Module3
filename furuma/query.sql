c22523f60658832fccd5cd27cf967a85f7814505
Doantaihuynh100983 <doantai100983hu@gmail.com>
2021-12-13 16:16:19 +0700

    up bai

--- furuma/query.sql
+++ furuma/query.sql
@@ -7,11 +7,12 @@ FROM
 WHERE
     ho_ten LIKE 'T%' OR ho_ten LIKE 'H%'
         OR ho_ten LIKE 'K%'
-        AND (LENGTH(ho_ten) <= 15);
-
-
--- CÂU 3 : Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và
--- có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
+        AND (LENGTH(ho_ten) <= 15) ;
+        
+        
+        
+-- câu 3  Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và
+-- có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.  
 SELECT 
     *
 FROM
@@ -63,11 +64,11 @@ SELECT
   SUM( ifnull(chi_phi_thue,0) + (ifnull(hdct.so_luong * dvdk.gia,0)) )  AS tong_tien
 FROM
     furuma.khach_hang kh
-      left  JOIN
+      left JOIN
     furuma.hop_dong hd ON hd.ma_khach_hang = kh.ma_khach_hang
        left JOIN
     furuma.loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach
-	  left  JOIN
+	  left JOIN
     furuma.dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
     left JOIN
    furuma.hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
@@ -82,10 +83,10 @@ ORDER BY kh.ma_khach_hang ASC;
 -- thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
 
 SELECT 
-    dv.ma_dich_vu,
+ dv.ma_dich_vu,
     dv.ten_dich_vu,
-    dv.dien_tich,
-    dv.chi_phi_thue,
+   dv.dien_tich,
+     dv.chi_phi_thue,
     ldv.ten_loai_dich_vu
 FROM
     furuma.loai_dich_vu ldv
@@ -96,11 +97,11 @@ FROM
 WHERE
   dv.ma_dich_vu NOT IN (
      SELECT 
-            hd.ma_dich_vu
+            hop_dong.ma_dich_vu
          FROM
            furuma.hop_dong
    WHERE
-		month(hd.ngay_lam_hop_dong)  BETWEEN 1 and 3
+		(month(hop_dong.ngay_lam_hop_dong)  BETWEEN 1 and 3 )
  )
 group by dv.ten_dich_vu
 order by dv.ma_dich_vu;
@@ -114,12 +115,121 @@ order by dv.ma_dich_vu;
 -- so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại
 -- dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng
 -- chưa từng được khách hàng đặt phòng trong năm 2021.
+SELECT 
+    dv.ma_dich_vu,
+    dv.ten_dich_vu,
+    dv.so_nguoi_toi_da,
+    dv.dien_tich,
+    dv.chi_phi_thue,
+    ldv.ten_loai_dich_vu
+FROM
+    furuma.dich_vu dv
+        JOIN
+    furuma.loai_dich_vu ldv
+        JOIN
+    furuma.hop_dong hd ON dv.ma_dich_vu = hd.ma_dich_vu
+WHERE
+    dv.ma_dich_vu NOT IN (
+    SELECT 
+            hop_dong.ma_dich_vu
+        FROM
+            furuma.hop_dong
+        WHERE
+            YEAR(hop_dong.ngay_lam_hop_dong) = 2021)
+      
+--   AND 
+--   
+--   dv.ma_dich_vu NOT IN (SELECT
+--             hd.ma_dich_vu
+--         FROM
+--             furuma.hop_dong
+--         WHERE
+--             YEAR(hd.ngay_lam_hop_dong) = 2021)
+
+	group by dv.ten_dich_vu;
 
 
-	
+
+-- câu 8 :Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu
+-- ho_ten không trùng nhau.
+-- cách 1 :
+select  DISTINCT  khach_hang.ho_ten 
+from furuma.khach_hang;
+-- cách 2
+select khach_hang.ho_ten
+from furuma.khach_hang
+group by khach_hang.ho_ten;
+-- cách 3 
+select khach_hang.ho_ten
+from furuma.khach_hang
+union 
+select khach_hang.ho_ten
+from furuma.khach_hang;
 
 
 
- select * from furuma.dich_vu;
+-- câu 9 : Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi
+-- tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt
+-- phòng.
+select month(hd.ngay_lam_hop_dong) AS 'Tháng' , count(month(hd.ngay_lam_hop_dong)) AS 'Số Lần'
+from furuma.khach_hang kh join furuma.hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang 
+ where year(hd.ngay_lam_hop_dong) = 2021
+ group by month(hd.ngay_lam_hop_dong)
+
+ order by month(hd.ngay_lam_hop_dong);
+ 
+ 
+ 
+ 
+-- câu 10 :Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu
+-- dịch vụ đi kèm. Kết quả hiển thị bao gồm ma_hop_dong,
+-- ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc,
+-- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở
+-- dich_vu_di_kem).
+select hd.ma_hop_dong , hd.ngay_lam_hop_dong , hd.ngay_ket_thuc , hd.tien_dat_coc , ifnull(sum(hdct.so_luong),0) 'Số luong dịch vu đi kèm'
+from furuma.hop_dong hd  left join furuma.hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+ left join furuma.dich_vu_di_kem dvdk on  hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+group by hd.ma_hop_dong;
+
+
+
+
+
+-- Câu 11 Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách
+-- hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc
+-- “Quảng Ngãi”.
+select dvdk.ma_dich_vu_di_kem , dvdk.ten_dich_vu_di_kem
+from furuma.khach_hang kh join furuma.loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+		join furuma.hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang 
+        join  furuma.hop_dong_chi_tiet hdct on  hd.ma_hop_dong = hdct.ma_hop_dong
+        join furuma.dich_vu_di_kem  dvdk on  hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+where kh.dia_chi like "%Vinh%" or kh.dia_chi like "%Quảng Ngãi%" AND lk.ten_loai_khach like "%Diamond%";
+
+
+
+
+
+-- câu 12 
+-- Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách
+-- hàng), so_dien_thoai (khách hàng), ten_dich_vu,
+-- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở
+-- dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ đã từng được
+-- khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được
+-- khách hàng đặt vào 6 tháng đầu năm 2021.
+select hd.ma_hop_dong , nv.ho_ten , kh.ho_ten , kh.so_dien_thoai , dv.ma_dich_vu , dv.ten_dich_vu , ifnull(sum(hdct.so_luong),0) 'số lượng dịch vụ đi kèm' , hd.tien_dat_coc
+from furuma.hop_dong hd join furuma.nhan_vien nv on hd.ma_nhan_vien = nv.ma_nhan_vien
+	join furuma.khach_hang kh on hd.ma_khach_hang = kh.ma_khach_hang
+    join furuma.dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
+	left join furuma.hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+where (year(hd.ngay_lam_hop_dong) = 2020 and month(hd.ngay_lam_hop_dong) IN (10 , 11, 12))
+        AND (month(hd.ngay_lam_hop_dong) NOT  IN (1,2,3,4,5,6) and year(hd.ngay_lam_hop_dong) = 2020) 
+group by hd.ma_hop_dong
+
+
+
+
+-- câu 13 :Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các
+-- Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử
+-- dụng nhiều như nhau).
 
 
