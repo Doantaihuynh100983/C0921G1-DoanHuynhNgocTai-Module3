08778a9bd9eecc653ce4864411ba44ab63db46d2
Doantaihuynh100983 <doantai100983hu@gmail.com>
2021-12-14 16:50:29 +0700

    up bai

--- furuma/query.sql
+++ furuma/query.sql
@@ -101,7 +101,7 @@ WHERE
          FROM
            furuma.hop_dong
    WHERE
-		(month(hop_dong.ngay_lam_hop_dong)  BETWEEN 1 and 3 )
+		(month(hop_dong.ngay_lam_hop_dong)  BETWEEN 1 and 3 ) and year(hop_dong.ngay_lam_hop_dong) = 2021
  )
 group by dv.ten_dich_vu
 order by dv.ma_dich_vu;
@@ -267,5 +267,52 @@ having (count(hd.ma_nhan_vien) <=3)
 order by  hd.ma_nhan_vien;
 
 
+
+-- Câu 16 : Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019
+-- đến năm 2021.
+SELECT * FROM furuma.nhan_vien;
+DELETE FROM furuma.nhan_vien
+WHERE nhan_vien.ma_nhan_vien NOT IN (SELECT hop_dong.ma_nhan_vien
+    FROM furuma.hop_dong
+    WHERE YEAR(ngay_lam_hop_dong) BETWEEN 2019 AND 2021);
+	
+
+
+-- câu 17 : Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum
+-- lên Diamond, chỉ cập nhật những khách hàng đã từng đặt phòng với
+-- Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ.
+
+
+
+
+
+
+
+-- câu 18 : Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc
+-- giữa các bảng).
+
+-- xóa có hai cách 1 là dùng  SET FOREIGN_KEY_CHECKS = 0 phá vỡ logic của hệ thống 
+-- cách 2 là dùng  ON DELETE CASCADE khai báo trong tất cả các table có liên quan
+
+SELECT * FROM furuma.khach_hang;
+SET FOREIGN_KEY_CHECKS = 0;
+DELETE FROM furuma.khach_hang 
+WHERE khach_hang.ma_khach_hang IN (
+SELECT hop_dong.ma_khach_hang FROM furuma.hop_dong
+    WHERE YEAR(ngay_lam_hop_dong) < 2021
+);
+
+
+-- câu 19 :Cập nhật giá cho các dịch vụ đi kèm được sử dụng trên 10 lần trong
+-- năm 2020 lên gấp đôi.
+	
+    
+    
+    
+    
+
+-- câu 20 :Hiển thị thông tin của tất cả các nhân viên và khách hàng có trong hệ
+-- thống, thông tin hiển thị bao gồm id (ma_nhan_vien, ma_khach_hang),
+-- ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi.
 
 
